5ce2e860ea73e3f3931ecb324d0fc5cec9f43429
Salokya <kumar.salokya007@gmail.com>
2023-03-25 00:55:34 +0530

    jwt

---
+++ src/main/java/com/erp/auth/entity/UserInfo.java
@@ -0,0 +1,21 @@
+package com.erp.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "USERS")
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+}
